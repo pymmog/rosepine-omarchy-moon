b6ce534289bb8b3725e6b0ac921dbcaee6fc1f29
pymmog <pymmog@icloud.com>
2026-02-19 14:45:18 +0100

    Initial commit

---
+++ neovim.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		priority = 1000,
+		opts = {
+			variant = "moon",
+		},
+	},
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "rose-pine",
+		},
+	},
+}
